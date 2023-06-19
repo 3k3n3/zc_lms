36ca8c812c5cdb8f81044f80d8679114a85d91da
Ekene C <clarencekene@outlook.com>
2023-06-19 05:56:23 +0100

    debug

--- lms_project/settings.py
+++ lms_project/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
